11c81cdca17d70417774219e09814fd38c9d5151
narasu <58448900+narasu@users.noreply.github.com>
2020-07-31 16:06:17 +0200

    2d/3d scenes, 3d player

---
+++ Assets/Scripts/3D/Player/Player3D.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    private static Player3D instance;
+    public static Player3D Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<Player3D>();
+
+            return instance;
+        }
+    }
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool grounded;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        grounded = controller.isGrounded;
+        if (grounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float horizInput = Input.GetAxisRaw("Horizontal");
+        float vertInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        //normalize the movement vector to prevent super fast diagonal movement
+        Vector3 movement = Vector3.Normalize(forwardMovement + rightMovement);
+
+        controller.Move(movement * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    //[Header("Movement")]
+
+    
+
+    //[Header("Jumping")]
+    //[SerializeField] private float jumpForce;
+    //[SerializeField] private KeyCode jumpKey;
+    //private bool isJumping;
+
+    //private CharacterController charController;
+    //private Rigidbody rigidBody;
+
+    //private void Awake()
+    //{
+    //    charController = GetComponent<CharacterController>();
+    //    rigidBody = GetComponent<Rigidbody>();
+    //}
+
+    //private void Start()
+    //{
+
+    //}
+
+    //private void Update()
+    //{
+    //    PlayerMove();
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    if (Input.GetKeyDown(jumpKey))
+    //    {
+    //        isJumping = true;
+    //        Debug.Log("Jumping");
+    //        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    //    }
+    //}
+
+    ////handle player movement with input from the horizontal and vertical axes
+    //private void PlayerMove()
+    //{
+    //    float horizInput = Input.GetAxisRaw(horizontalInputName);
+    //    float vertInput = Input.GetAxisRaw(verticalInputName);
+
+    //    Vector3 forwardMovement = transform.forward * vertInput;
+    //    Vector3 rightMovement = transform.right * horizInput;
+
+    //    //normalize the movement vector to prevent super fast diagonal movement
+    //    Vector3 movement = Vector3.Normalize(forwardMovement + rightMovement) * movementSpeed;
+
+    //    charController.SimpleMove(movement);
+    //}
+
+
+    //jump movement
+    //private IEnumerator JumpEvent()
+    //{
+    //    charController.slopeLimit = 90.0f;
+    //    float timeInAir = 0.0f;
+    //    do
+    //    {
+    //        float jumpForce = jumpFallOff.Evaluate(timeInAir);
+    //        charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+    //        timeInAir += Time.deltaTime;
+    //        yield return null;
+    //    } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+    //    charController.slopeLimit = 45.0f;
+    //    isJumping = false;
+    //}
+
+
+}
---
+++ Assets/Scripts/3D/Player/PlayerLook.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+
+    [SerializeField] private float mouseSensitivity = 150f;
+#pragma warning restore 0649
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void LockCursor()
+    {
+        //hides and locks cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void CameraRotation()
+    {
+        //set mouse movement values
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+
+        if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
